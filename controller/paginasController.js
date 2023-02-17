0ff6ea8e8d4f9a16fa73c8f21c8a74dd63a9c49b
Javier <javiernr0599@gmail.com>
2023-02-17 00:47:51 -0400

    primer commit

---
+++ controller/paginasController.js
@@ -0,0 +1,88 @@
+import { Viaje } from '../models/Viaje.js';
+import { Testimonial } from '../models/Testimoniales.js';
+
+const paginaInicio = async (req, res) => { // req - lo que enviamos || res - lo que express nos responde
+
+    // Consultar 3 viajes de la base de datos y 3 testimoniales
+
+    const promiseDB = [];
+
+    promiseDB.push(Viaje.findAll({ limit: 3 }));
+    promiseDB.push(Testimonial.findAll({ limit: 3 }));
+
+    try {
+        const resultado = await Promise.all( promiseDB );
+
+        res.render('inicio', {
+            pagina: 'Inicio',
+            clase: 'home',
+            viajes: resultado[0],
+            testimoniales: resultado[1]
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+
+;}
+
+const paginaNosotros = (req, res) => { 
+
+    res.render('nosotros', {
+        pagina: 'Nosotros'
+    });
+;}
+
+const paginaViajes = async (req, res) => { 
+    // Consultar la BD
+
+    const viajes = await Viaje.findAll();
+
+    console.log(viajes);
+
+    res.render('viajes', {
+        pagina: 'Próximos Viajes',
+        viajes,
+    });
+};
+
+const paginaTestimoniales =  async (req, res) => { 
+
+    try {
+        const testimoniales = await Testimonial.findAll();
+
+        res.render('testimoniales', {
+            pagina: 'Testimoniales',
+            testimoniales
+        });
+    } catch (error) {
+        console.log(error);
+    }
+
+};
+
+// Muestraun viaje por su slug
+const paginaDetalleViaje = async  (req, res) => {
+    const { slug } = req.params;
+
+    try {
+        const viaje = await Viaje.findOne({ where : {slug} })
+
+        res.render('viaje', {
+            pagina: 'Información Viaje',
+            viaje
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+export {
+    paginaInicio,
+    paginaNosotros,
+    paginaViajes,
+    paginaTestimoniales,
+    paginaDetalleViaje
+}
