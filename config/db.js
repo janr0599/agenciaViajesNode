3f35f3fd83b940e09a8bcd53c5e05d71357733d7
Javier <javiernr0599@gmail.com>
2023-02-19 00:11:15 -0400

    first commit

--- config/db.js
+++ config/db.js
@@ -1,13 +1,12 @@
 import Sequelize from 'sequelize';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
 dotenv.config()
-
 console.log(process.env.DB_HOST);
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
-    port: '3306',
+    port: process.env.DB_PORT,
     dialect: 'mysql',
     define: {
         timestamps: false
@@ -16,7 +15,7 @@ const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.D
         max: 5,
         min: 0,
         acquire: 30000,
-        idel: 10000
+        idle: 10000
     },
     operatorAliases: false
 });
